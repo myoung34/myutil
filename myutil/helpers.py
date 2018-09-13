fdad62a32a909b2c9dba7009d866155e3b734b28
Marcus Young <myoung34@my.apsu.edu>
2018-09-13 14:38:53 -0500

    Add documentation and LICENSE file

--- myutil/helpers.py
+++ myutil/helpers.py
@@ -4,6 +4,11 @@ import os
 
 
 def mkdir_p(path):
+    """Helper method to mkdir recursively (similar to `mkdir -p`
+
+    Keyword arguments:
+    path -- string path to create
+    """
     try:
         os.makedirs(path)
     except OSError as exc:
